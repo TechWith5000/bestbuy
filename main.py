6b654d51b0129457a0fc17e07b793ccc8d498bc1
Jan-Philipp Gutt <janphilippgutt5000@gmail.com>
2024-12-29 20:37:53 +0100

    Established Store class

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 from products import Product
+from store import Store
 
 def main():
     '''Test code'''
-    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-    mac = Product("MacBook Air M2", price=1450, quantity=100)
+    product_list = [Product("MacBook Air M2", price=1450, quantity=100),
+                    Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    Product("Google Pixel 7", price=500, quantity=250),
+                    ]
 
-    print(bose.buy(50))
-    try:
-        mac.buy(101)
-    except ValueError as e:
-        print(e)
-    #print(mac.buy(101))
-    print(mac.is_active())
-
-    bose.show()
-    mac.show()
-
-    bose.set_quantity(1000)
-    bose.show()
+    store = Store(product_list)
+    products = store.get_all_products()
+    print(store.get_total_quantity())
+    print(store.order([(products[0], 1), (products[1], 2)]))
 
 if __name__ == "__main__":
     main()
